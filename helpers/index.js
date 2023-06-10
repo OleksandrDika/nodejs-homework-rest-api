c22ab2263485e36f340c79e33c83e05030048b7b
OleksandrDika <109437683+OleksandrDika@users.noreply.github.com>
2023-06-10 17:01:09 +0300

    done send email

--- helpers/index.js
+++ helpers/index.js
@@ -2,9 +2,11 @@ const handleMongooseError = require("./handleMongooseError");
 const HttpError = require("./HttpError");
 
 const ctrlWrapper = require("./ctrlWrapper");
+const sendMail = require("./sendEmail");
 
 module.exports = {
   HttpError,
   handleMongooseError,
   ctrlWrapper,
+  sendMail,
 };
